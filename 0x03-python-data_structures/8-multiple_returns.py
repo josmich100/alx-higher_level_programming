256f90b49a6bcbd66867e2b01ca0318fc0ca8f89
josmich100 <michinajosiah@gmail.com>
2023-08-14 13:45:15 -0700

    sixth

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if not sentence:
+        first = None
+    else:
+        first = sentence[0]
+    length = len(sentence)
+    return length, first
