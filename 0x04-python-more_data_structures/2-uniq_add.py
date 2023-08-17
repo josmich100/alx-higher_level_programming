f804104fd5220c3aef12ce52e849c83c39090ddf
josmich100 <michinajosiah@gmail.com>
2023-08-16 19:02:41 -0700

    starrtue

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    unique_numbers = set()
+    total = 0
+
+    for num in my_list:
+        if num not in unique_numbers:
+            total += num
+            unique_numbers.add(num)
+
+    return total
+
