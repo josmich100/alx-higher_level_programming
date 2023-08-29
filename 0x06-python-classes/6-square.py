2338a5cffd104d7024708961fc83f4ff41eb000a
josmich100 <michinajosiah@gmail.com>
2023-08-29 21:46:54 +0000

    starye

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,53 +1,112 @@
 #!/usr/bin/python3
+"""Class Square defines a square"""
+
 
 class Square:
-    """Defines a square."""
+    """This class defines a square.
 
+    This class has no public attributes.
+
+    """
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize the Square instance with the given size and position."""
-        self.size = size
-        self.position = position
+        """This method initiates a square.
+
+        Args:
+            size (int): This defines the size of the square.
+                The size is validated in the setter method.
+            position (tuple): This defines the position of the square.
+                The position is validated in the setter method
+
+        """
+        try:
+            self.__size = size
+            if size < 0:
+                raise ValueError
+            if type(size) is not int:
+                raise TypeError
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be >= 0")
+        try:
+            self.__position = position
+            if type(position) is not tuple:
+                raise TypeError
+            if len(position) != 2:
+                raise TypeError
+            if position[0] < 0 or position[1] < 0:
+                raise TypeError
+        except TypeError:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
     def size(self):
-        """Getter method for retrieving the size."""
+        """This method retrieves the size of a square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter method for setting the size."""
-        if not isinstance(value, int):
+        """This method sets the size of a square.
+
+        Args:
+            size (int): This defines the size of the square.
+                The size is validated with try/except.
+
+        """
+        try:
+            self.__size = value
+            if value < 0:
+                raise ValueError
+            if type(value) is not int:
+                raise TypeError
+        except TypeError:
             raise TypeError("size must be an integer")
-        if value < 0:
+        except ValueError:
             raise ValueError("size must be >= 0")
-        self.__size = value
 
     @property
     def position(self):
-        """Getter method for retrieving the position."""
+        """This method retrieves the position of a square."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Setter method for setting the position."""
-        if not isinstance(value, tuple) or len(value) != 2 or \
-                not all(isinstance(val, int) for val in value) or \
-                not all(val >= 0 for val in value):
+        """This method sets the position of a square
+
+        Args:
+            position: this tuple defines the position of the square.
+                The position is validated with try/except.
+
+        """
+        try:
+            self.__position = value
+            if type(value) is not tuple:
+                raise TypeError
+            if len(value) != 2:
+                raise TypeError
+            if value[0] < 0 or value[1] < 0:
+                raise TypeError
+        except TypeError:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     def area(self):
-        """Compute and return the area of the square."""
+        """int: Return area of square."""
         return self.__size * self.__size
 
     def my_print(self):
-        """Print the square with '#' characters and position."""
-        if self.__size == 0:
+        """Print the square"""
+        s = self.__size
+        pos = self.__position
+        p1 = pos[0]
+        p2 = pos[1]
+        if s == 0:
             print()
-            return
-        for _ in range(self.__position[1]):
-            print()
-        for _ in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
-
+        else:
+            for ii in range(p2):
+                    print()
+            for i in range(s):
+                for j in range(p1):
+                    print(" ", end='')
+                for j in range(s):
+                    print("#", end='')
+                print()
