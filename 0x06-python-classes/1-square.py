7333998ec1f7a6fb0902ff8997e42d2918f3c4a8
josmich100 <michinajosiah@gmail.com>
2023-08-29 20:42:29 +0000

    freaser

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialize the Square instance with the given size."""
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize the Square instance with the given size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
