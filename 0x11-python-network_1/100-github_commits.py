16a95f5b0bff1255611ede7cbb8d597ca332f6c1
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-12-02 00:54:47 +0300

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,19 +1,24 @@
-# !/usr/bin/python3
-import sys
-import requests
+#!/usr/bin/python3
+"""
+Uses GitHub API to display the user id
+"""
 
+import requests
+import sys
 
 if __name__ == "__main__":
-    # user Github API to list 10 commits from specific user in specific REPO
-    try:
-        REPO = sys.argv[1]
-        OWNER = sys.argv[2]
-        URL = f'https://api.github.com/repos/{OWNER}/{REPO}/commits'
-        r = requests.get(URL)
-        r_json = r.json()
-        LENGTH = len(r_json)
-        if LENGTH == min(LENGTH, 10):
-            for i in range(LENGTH):
-                print(f"{r_json[i].get('sha')}: {r_json[i].get('commit').get('author').get('name')}")
-    except:
-        print("None")
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        password = sys.argv[2]
+
+        url = 'https://api.github.com/user'
+
+        response = requests.get(url, auth=(username, password))
+
+        try:
+            data = response.json()
+            print(data.get('id'))
+        except ValueError:
+            print("None")
+    else:
+        print("Usage: ./10-my_github.py <username> <password>")
