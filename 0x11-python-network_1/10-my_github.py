e3b1f96ee35ab4a465da7bc3fdcda5a039ce714a
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-12-02 00:53:30 +0300

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,24 @@
-# !/usr/bin/python3
-import sys
-import requests
+#!/usr/bin/python3
+"""
+Uses GitHub API to display the user id
+"""
 
+import requests
+import sys
 
 if __name__ == "__main__":
-    # uses Github credentials to display ID
-    try:
-        USERNAME = sys.argv[1]
-        PASSWORD = sys.argv[2]
-        URL = 'https://api.github.com/user'
-        r = requests.get(URL, auth=(USERNAME, PASSWORD))
-        json_r = r.json()
-        print(json_r.get('id'))
-    except:
-        print("None")
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        password = sys.argv[2]
+
+        url = 'https://api.github.com/user'
+
+        response = requests.get(url, auth=(username, password))
+
+        try:
+            data = response.json()
+            print(data.get('id'))
+        except ValueError:
+            print("None")
+    else:
+        print("Usage: ./10-my_github.py <username> <password>")
