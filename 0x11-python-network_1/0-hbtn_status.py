e1d505c4ebf75204050d0eb03c91579e6f0c485f
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-12-02 00:29:44 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,11 @@
-# !/usr/bin/python3
-# Module with code that fetches a URL
-import urllib.request
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status using urllib
+"""
 
+from urllib import request
 
-if __name__ == "__main__":
-    # fetches a URL
-    URL = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(URL) as response:
-        html = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(html)}")
-    print(f"\t- content: {html}")
-    print(f"\t- utf8 content: {html.decode}")
+url = 'https://alx-intranet.hbtn.io/status'
+
+with request.urlopen(url) as response:
+    content = response.read()
