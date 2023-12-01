00efe63f5123f99f27b915a1164e1cde5fc979a5
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-12-02 00:48:57 +0300

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,20 @@
-# !/usr/bin/python3
-import sys
-import requests
+#!/usr/bin/python3
+"""
+Sends a POST request to a URL with an email parameter and displays the body of the response.
+"""
 
+import requests
+import sys
 
 if __name__ == "__main__":
-    # posts an email
-    email = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data=email)
-    print(r.text)
+    if len(sys.argv) == 3:
+        url = sys.argv[1]
+        email = sys.argv[2]
+
+        # Data to be sent in the POST request
+        data = {'email': email}
+
+        response = requests.post(url, data=data)
+        print(response.text)
+    else:
+        print("Usage: ./6-post_email.py <URL> <email>")
