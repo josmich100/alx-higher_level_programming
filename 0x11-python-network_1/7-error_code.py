8e7f6d75933280fcdd66a2d9a1ca3d65af873c8b
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-12-02 00:49:55 +0300

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,22 @@
-# !/usr/bin/python3
-import sys
-import requests
+#!/usr/bin/python3
+"""
+Sends a request to a URL and displays the body of the response.
+Prints an error message if the HTTP status code is greater than or equal to 400.
+"""
 
+import requests
+import sys
 
 if __name__ == "__main__":
-    # handles error codes
-    r = requests.get(sys.argv[1])
-    if r.status_code >= 400:
-        print(f"Error code: {r.status_code}")
+    if len(sys.argv) == 2:
+        url = sys.argv[1]
+
+        response = requests.get(url)
+        status_code = response.status_code
+
+        if status_code >= 400:
+            print(f"Error code: {status_code}")
+        else:
+            print(response.text)
     else:
-        print(r.text)
+        print("Usage: ./7-error_code.py <URL>")
