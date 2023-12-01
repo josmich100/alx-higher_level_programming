e7a9fd8dc0f047b76aed0fd1a161186735a304f2
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-12-02 00:52:03 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,16 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status using the requests package
+"""
+
 import requests
 
-
 if __name__ == "__main__":
-    # fetches a specific website
-    URL = 'https://intranet.hbtn.io/status'
-    r = requests.get(URL)
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    response = requests.get(url)
+    body = response.text
+
     print("Body response:")
-    print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.text}")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
