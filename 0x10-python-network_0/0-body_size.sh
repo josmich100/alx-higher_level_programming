211030dc6058c66985ae475565b35f29a487f830
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-12-05 14:18:12 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays the size of the body of the response in bytes
-
-url=$1
-curl -sI "$url" | grep -i 'Content-Length' | awk '{print $2}'
+curl -s "$1" | wc -c
