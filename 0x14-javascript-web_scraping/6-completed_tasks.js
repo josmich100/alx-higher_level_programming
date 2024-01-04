bb3254e2fc6fbc1dc4133b4d55c3e03ce56ce0d0
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-01-04 19:27:10 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,7 @@
 //Write a script that computes the number of tasks completed by user id.
 //The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
 //You must use the module request
-const request = require("request");
+const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
