712d94b7851df54e442ad303283ee7bdd1afc9f1
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-01-04 19:43:06 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-//Write a script that computes the number of tasks completed by user id.
-//The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
-//You must use the module request
+// Write a script that computes the number of tasks completed by user id.
+// The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+// You must use the module request
 const request = require('request');
 const url = process.argv[2];
 
