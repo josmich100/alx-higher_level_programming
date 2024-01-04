d2d4acb1624c6a911817b1458215f49c0cc22580
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-01-04 19:28:45 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,7 @@
 // The first argument is the URL to request (GET)
 // The status code must be printed like this: code: <status code>
 // You must use the module request
-const request = require("request");
+const request = require('request');
 
 const url = process.argv[2];
 
