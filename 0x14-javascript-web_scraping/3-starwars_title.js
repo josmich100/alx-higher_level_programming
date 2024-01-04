e36c381975c24b9a87d0d1a68dafbe6337f7913f
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-01-04 19:26:04 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@
 // The first argument is the movie ID
 // You must use the Star wars API with the endpoint https://swapi-api.hbtn.io/api/films/:id
 // You must use the module request
-const request = require("request");
+const request = require('request');
 
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
