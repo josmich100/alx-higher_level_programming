45cac305f4fd7e639be445432e824a999aa896cc
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-01-04 19:26:50 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,8 +4,8 @@
 // The second argument the file path to store the body response
 // The file must be UTF-8 encoded
 // You must use the module request
-const request = require("request");
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
@@ -14,7 +14,7 @@ request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    fs.writeFile(filePath, body, "utf-8", (err) => {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
       if (err) {
         console.error(err);
       }
