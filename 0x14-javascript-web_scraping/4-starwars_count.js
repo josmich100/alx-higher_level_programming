f012c2a9439ed30903075595a6ecf60b4d8fbb2d
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-01-04 19:38:00 +0300

    udyr

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,23 +3,23 @@
 // The first argument is the API URL of the Star wars API: https://swapi-api.hbtn.io/api/films/
 // Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
 // You must use the module request
-const request = require('request');
+const request = require("request");
 
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-	  if (error) {
-	console.error(error);
+  if (error) {
+    console.error(error);
   } else {
-	const films = JSON.parse(body).results;
-	let count = 0;
-	for (const film of films) {
-	  for (const character of film.characters) {
-		if (character.endsWith('/18/')) {
-		  count++;
-		}
-	  }
-	}
-	console.log(count);
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.endsWith("/18/")) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
